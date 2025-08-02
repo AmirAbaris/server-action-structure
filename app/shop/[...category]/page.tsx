a8ae37417bf0a789f840b99e056ae6d4085867fc
Amir Abaris <thisisamirabaris@gmail.com>
2025-08-02 15:16:53 +0330

    Add Page component for dynamic category routing and brand display

---
+++ app/shop/[...category]/page.tsx
@@ -0,0 +1,24 @@
+type PageProps = {
+  params: Promise<{ category: string[] }>;
+  searchParams: Promise<{ brand?: string | string[] | undefined }>;
+};
+
+// the [...category] route captures all segments of the URL path as an array
+// e.g., /shop/electronics/phones will result in category = ['electronics', 'phones']
+export default async function Page(props: PageProps) {
+  const { category } = await props.params;
+  const brands = Array.isArray((await props.searchParams).brand)
+    ? (await props.searchParams).brand
+    : [(await props.searchParams).brand];
+
+  return (
+    <div>
+      <h1>Category: {category.join(" / ")}</h1>
+      <p>This page displays products in the category: {category.join(" > ")}</p>
+      <div>
+        Category: {category.join(" > ")} <br />
+        Brands: {brands}
+      </div>
+    </div>
+  );
+}
